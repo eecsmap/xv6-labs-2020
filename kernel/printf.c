87ffdf9ebb7fb9dffdf66ac853510cd215fcf230
wenyang1983 <wenyang.duan@gmail.com>
2021-06-11 00:12:49 -0700

    backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,15 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void backtrace(void)
+{
+  printf("backtrace:\n");
+  uint64 fp = r_fp();
+  uint64 up = PGROUNDUP(fp);
+  while (fp < up) {
+    printf("%p\n", ((uint64 *)fp)[-1]);
+    fp = ((uint64 *)fp)[-2];
+  };
 }

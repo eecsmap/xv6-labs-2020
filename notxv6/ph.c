9e250e20c0985b041c014d16ece05e0c1e60f110
wenyang1983 <wenyang.duan@gmail.com>
2021-06-18 21:07:35 -0700

    ph and barrier

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock[NBUCKET];            // declare a lock
+
 struct entry {
   int key;
   int value;
@@ -39,6 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&lock[i]);       // acquire lock
 
   // is the key already present?
   struct entry *e = 0;
@@ -53,19 +56,22 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&lock[i]);     // release lock
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&lock[i]);       // acquire lock
 
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&lock[i]);     // release lock
   return e;
 }
 
@@ -99,6 +105,9 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  for (int i = 0; i < NBUCKET; ++i)
+    pthread_mutex_init(&lock[i], NULL); // initialize the lock
+
   pthread_t *tha;
   void *value;
   double t1, t0;
